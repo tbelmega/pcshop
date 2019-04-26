94f1a4e58f046754adce5bca2e5213586f0c6b56
tbelmega <thiemo.belmega@gmail.com>
2019-04-26 22:32:01 +0200

    Add JUnit and hamcrest tests to ProductRepositoryTest

--- src/main/kotlin/de/oncoding/pcshop/product/ProductRepository.kt
+++ src/main/kotlin/de/oncoding/pcshop/product/ProductRepository.kt
@@ -1,9 +1,17 @@
 package de.oncoding.pcshop.product
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ProductRepository: JpaRepository<Product, String> {
+
+    /**
+     * Overridden to make tests fail. Delete all of this to make the tests pass.
+     * The "LIMIT 1" causes the result to have never more than 1 record
+     */
+    @Query(nativeQuery = true, value = "SELECT * FROM product LIMIT 1")
+    override fun findAll(): List<Product>
 
 }
--- src/test/kotlin/de/oncoding/pcshop/product/ProductRepositoryTest.kt
+++ src/test/kotlin/de/oncoding/pcshop/product/ProductRepositoryTest.kt
@@ -1,6 +1,9 @@
 package de.oncoding.pcshop.product
 
 import org.assertj.core.api.Assertions.assertThat
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.Matchers.containsInAnyOrder
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,7 +14,116 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner::class)
 @DataJpaTest
-class ProductRepositoryTest {
+class ProductRepositoryTestJUnit {
+
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    @Test
+    fun `saves product in database and loads by id`() {
+        // given
+        val productToSave = Product("1", "Ryzen 7 2700X", "AMD")
+
+        // when
+        productRepository.save(productToSave)
+
+        // then
+        val loadedProduct = productRepository.getOne("1")
+        assertEquals(productToSave, loadedProduct)
+    }
+
+    @Test
+    fun `loads all products from database (1)`() {
+        // given
+        val product1 = productRepository.save(Product("1", "Ryzen 7 2700X", "AMD"))
+        val product2 = productRepository.save(Product("2", "Core i9 9900K", "Intel"))
+
+        // when
+        val products = productRepository.findAll()
+
+        // then
+        assertTrue(products.contains(product1))
+        assertTrue(products.contains(product2))
+        assertEquals(2, products.size)
+    }
+
+    @Test
+    fun `loads all products from database (2)`() {
+        // given
+        val product1 = productRepository.save(Product("1", "Ryzen 7 2700X", "AMD"))
+        val product2 = productRepository.save(Product("2", "Core i9 9900K", "Intel"))
+
+        // when
+        val products = productRepository.findAll()
+
+        // then
+        assertTrue("Loaded products should contain product 1", products.contains(product1))
+        assertTrue("Loaded products should contain product 2", products.contains(product2))
+        assertEquals(2, products.size)
+    }
+
+    @Test
+    fun `loads all products from database (3)`() {
+        // given
+        val product1 = productRepository.save(Product("1", "Ryzen 7 2700X", "AMD"))
+        val product2 = productRepository.save(Product("2", "Core i9 9900K", "Intel"))
+
+        // when
+        val products = productRepository.findAll()
+
+        // then
+        assertTrue("Loaded products should contain excatly product 1 and 2",
+                containsExactlyInAnyOrder(products, product1, product2)
+        )
+    }
+
+    private fun containsExactlyInAnyOrder(actual: List<Product>, vararg expected: Product): Boolean {
+        return (actual.size == expected.size)
+                && actual.containsAll(expected.asList())
+                && expected.asList().containsAll(actual)
+    }
+}
+
+
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner::class)
+@DataJpaTest
+class ProductRepositoryTestHamcrest {
+
+    @Autowired
+    private lateinit var productRepository: ProductRepository
+
+    @Test
+    fun `saves product in database and loads by id`() {
+        // given
+        val productToSave: Product = Product("1", "Ryzen 7 2700X", "AMD")
+
+        // when
+        productRepository.save(productToSave)
+
+        // then
+        val loadedProduct: Product = productRepository.getOne("1")
+        assertThat(loadedProduct, equalTo(productToSave))
+    }
+
+    @Test
+    fun `loads all products from database`() {
+        // given
+        val product1 = productRepository.save(Product("1", "Ryzen 7 2700X", "AMD"))
+        val product2 = productRepository.save(Product("2", "Core i9 9900K", "Intel"))
+
+        // when
+        val products = productRepository.findAll()
+
+        // then
+        assertThat(products, containsInAnyOrder(product1, product2)) // hamcrest containsInAnyOrder corresponds to assertJs containsExactlyInAnyOrder, while hamcrest hasItems corresponds to assertJ contains
+    }
+}
+
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner::class)
+@DataJpaTest
+class ProductRepositoryTestAssertJ {
 
     @Autowired
     private lateinit var productRepository: ProductRepository
